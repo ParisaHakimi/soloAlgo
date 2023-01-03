fe603868f9b328b5f71a426fdf88695ae8c733ee
Parisa <parisahs@gmail.com>
2023-01-03 15:14:37 -0500

    printIntsAndSum0To255

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 // 1- Print 1-255  Print all the integers from 1 to 255.
-
 // function print1To255() {
 //   for (let num = 1; num <= 255; num++) {
 //     console.log(num);
@@ -7,11 +6,21 @@
 // }
 // print1To255();
 
-//2. Print Odds 1-255   Print all odd integers from 1 to 255.
 
-function printOdds1To255() {
-  for (let num = 1; num <= 255; num += 2) {
-    console.log(num);
+//2. Print Odds 1-255   Print all odd integers from 1 to 255.
+// function printOdds1To255() {
+//   for (let num = 1; num <= 255; num += 2) {
+//     console.log(num);
+//   }
+// }
+// printOdds1To255()
+
+// 3. Print Ints and Sum 0-255   Print integers from 0 to 255, and with each integer print the sum so far. 
+function printIntsAndSum0To255(){
+  let sum=0
+  for(let num=0;num<=255;num++){
+    console.log("number is: ",num,"sum up to here is: ",sum+=num)
+
   }
 }
-printOdds1To255()
+printIntsAndSum0To255()
