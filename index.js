350eb4be68265450cd2c5e015b68cc23e3e525eb
Parisa <parisahs@gmail.com>
2023-01-03 15:22:42 -0500

    printMaxOfArray

--- index.js
+++ index.js
@@ -27,10 +27,24 @@
 
 
 // 4. Iterate and Print Array   Iterate through a given array, printing each value. 
-function printArrayVals(arr){
-for(let element of arr){
-  console.log(element)
+// function printArrayVals(arr){
+// for(let element of arr){
+//   console.log(element)
+// }
+// }
+// const arr=[1,34,"hello"]
+// printArrayVals(arr)
+
+
+//5. Find and Print Max      Given an array, find and print its largest element. 
+function printMaxOfArray(arr){
+  let largest=0
+  for(let element of arr){
+    if(element>largest){
+      largest=element
+    }
+    console.log("largest is: ",largest)
+  }
 }
-}
-const arr=[1,34,"hello"]
-printArrayVals(arr)
+const arr=[2,33,1,42,90,45]
+printMaxOfArray(arr)
