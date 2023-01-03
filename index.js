e489d9eee85504dae8ac82fbd3ee3e3e55f0511b
Parisa <parisahs@gmail.com>
2023-01-03 15:05:39 -0500

    printOdds1To255

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 // 1- Print 1-255  Print all the integers from 1 to 255.
 
-function print1To255() {
-  for (let num = 1; num <= 255; num++) {
+// function print1To255() {
+//   for (let num = 1; num <= 255; num++) {
+//     console.log(num);
+//   }
+// }
+// print1To255();
+
+//2. Print Odds 1-255   Print all odd integers from 1 to 255.
+
+function printOdds1To255() {
+  for (let num = 1; num <= 255; num += 2) {
     console.log(num);
   }
 }
-print1To255();
+printOdds1To255()
